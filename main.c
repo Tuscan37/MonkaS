af8862b78f47d7c202831e2e1c4626de370d935d
Chyrek <malewolans123@gmail.com>
2020-01-11 20:17:01 +0100

    ABC DDD

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include 'sum.h'
+#include 'subt.h'
+#include 'mult.h'
+#include 'div.h'
+
+int main()
+{
+    float a;
+    float b;
+    printf("Wprowadz 2 liczby:\n");
+    scanf("%f", &a);
+    scanf("%f", &b);
+    sum(a , b);
+    subt(a , b);
+    mult(a , b);
+    div(a , b);
+  
+}
